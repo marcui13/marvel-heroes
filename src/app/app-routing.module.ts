a8e9980ed03cd45bcbfa9f5c0caa6745a1b5cab1
Agustín <amarquardt20@gmail.com>
2024-05-09 12:37:07 -0300

    se actualiza app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'heroes-list',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'heroes-list',
+    loadChildren: () => import('./@pages/heroes-list/heroes-list.module').then( m => m.HeroesListPageModule)
+  },
+  {
+    path: 'hero-details',
+    loadChildren: () => import('./@pages/hero-details/hero-details.module').then( m => m.HeroesDetailsPageModule)
   },
 ];
 
