37dcec56bef746a15b10253065ade7ced75be736
Agustín <amarquardt20@gmail.com>
2024-05-09 14:51:22 -0300

    inicialización de lenguaje en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private translate: TranslateService) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    // Establece el idioma predeterminado
+    this.translate.setDefaultLang('es');
+    // Establece el idioma actual según el idioma del dispositivo
+    const browserCurrentLang = this.translate.getBrowserLang();
+    console.log('idioma actual del dispositivo:', browserCurrentLang)
+    if(browserCurrentLang){
+      this.translate.use(browserCurrentLang.match(/en|es/) ? browserCurrentLang : 'es');
+    }
+  }
 }
