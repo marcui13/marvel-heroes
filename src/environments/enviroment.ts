f019d0522a0c417f4fc698dddf8c1053fbee1231
Agustín <amarquardt20@gmail.com>
2024-05-09 01:12:06 -0300

    configuración de enviroment

---
+++ src/environments/enviroment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    marvelApiBaseUrl: 'https://gateway.marvel.com/v1/public',
+    marvelApiPublicKey: '821eae7a8cb20aa4b027e949d1aace4b',
+    marvelApiPrivateKey: '1b7873c8054e8e27031b8e9460b50afcbea48a70',
+};
